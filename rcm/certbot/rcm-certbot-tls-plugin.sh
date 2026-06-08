94646acae01e70055b6ac7e76fe35b461283a8b5
ijortengab <m_roji28@yahoo.com>
2026-06-08 10:28:07 +0700

    Update massal. Hapus variable $RCM_INDENT.

--- rcm/certbot/rcm-certbot-tls-plugin.sh
+++ rcm/certbot/rcm-certbot-tls-plugin.sh
@@ -119,7 +119,6 @@ esac
 
 # Define variables and constants.
 [ -z "$fast" ] && fast="$RCM_FAST"; [ "$fast" == 0 ] && fast=
-RCM_INDENT='    '; [ "$(tput cols)" -le 80 ] && RCM_INDENT='  '
 [ -z "$fast" ] && isfast='' || isfast=' --fast'
 if [ -n "$RCM_VERBOSE" ];then
     verbose="$RCM_VERBOSE"
