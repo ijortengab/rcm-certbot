1d7c70a4af1e04f6cc562ec3ac029182652a432a
ijortengab <m_roji28@yahoo.com>
2026-06-29 10:47:29 +0700

    [acme-client/certbot/init/socket.sh] Bugfix, PATH belum ke load pada current shell.

--- rcm/install/interfaces/acme-client/implements/certbot/methods/init/socket.sh
+++ rcm/install/interfaces/acme-client/implements/certbot/methods/init/socket.sh
@@ -3,3 +3,8 @@
 INDENT+="$RCM_INDENT" \
 rcm certbot init \
     ; [ ! $? -eq 0 ] && x
+
+# Tambahkan path.
+command -v "certbot" >/dev/null || {
+    [ -f /etc/profile.d/apps-bin-path.sh ] && . /etc/profile.d/apps-bin-path.sh
+}
