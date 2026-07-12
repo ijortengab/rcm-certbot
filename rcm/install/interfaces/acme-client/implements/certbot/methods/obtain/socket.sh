81d3fab4655e173815f69884bf988633a7dfd458
ijortengab <m_roji28@yahoo.com>
2026-07-12 12:56:15 +0700

    [acme-client/certbot/obtain/socket.sh] Bugfix. Jika certbot sudah terinstall tapi belum ada di PATH.

--- rcm/install/interfaces/acme-client/implements/certbot/methods/obtain/socket.sh
+++ rcm/install/interfaces/acme-client/implements/certbot/methods/obtain/socket.sh
@@ -3,6 +3,11 @@
 # Dependency.
 [ -n "$RCM_FQDN" ] || { red "Unable to proceed, variable \$RCM_FQDN is empty."; x; }
 
+# Prepare PATH.
+command -v certbot >/dev/null || {
+    [ -f /etc/profile.d/apps-bin-path.sh ] && . /etc/profile.d/apps-bin-path.sh
+}
+
 domain_1="$RCM_FQDN"
 
 INDENT+="$RCM_INDENT" \
